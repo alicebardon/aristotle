773f1bfc153c1d9eddd61e721d8d4031eb3dd75f
Alice Bardon <abardon.catineau@gmail.com>
2023-07-30 18:34:30 +0200

    created folder to store logos and it works

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -2,6 +2,7 @@ class StartupsController < ApplicationController
   require 'net/http'
   require 'uri'
   require 'zip'
+  require 'fileutils'
 
   API_KEY = "2a28d17c7d25458a32690f9869580391"
 
@@ -15,13 +16,17 @@ class StartupsController < ApplicationController
 
     @startups = fetch_multiple_startups
 
+    # Create the folder "Aristotle_logos" if it doesn't exist
+    folder_name = 'Aristotle_logos'
+    FileUtils.mkdir_p(folder_name)
+
     @startups.each do |startup|
       logo_url = "https://images.crunchbase.com/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/#{startup[:logo]}"
       # Specify the destination filename for the downloaded image
-      image_filename = "#{startup[:name]}.jpg"
+      image_filename = File.join(folder_name, "#{startup[:name]}.jpg")
 
       # Specify the filename for the zip file
-      zip_filename = "#{startup[:name]}.zip"
+      # zip_filename = "#{startup[:name]}.zip"
 
       # Use URI.parse to get the actual image URL (follow redirects if any)
       actual_image_url = URI.parse(logo_url).to_s
@@ -30,26 +35,8 @@ class StartupsController < ApplicationController
       download_image(actual_image_url, image_filename)
 
       # Create a zip file and add the downloaded image to it
-      create_zip_file(image_filename, zip_filename)
+      # create_zip_file(image_filename, zip_filename)
     end
-
-    # # Specify the destination filename for the downloaded image
-    # image_filename = 'logo.jpg'
-
-    # # Specify the filename for the zip file
-    # zip_filename = 'logo.zip'
-
-    # # Use URI.parse to get the actual image URL (follow redirects if any)
-    # actual_image_url = URI.parse(logo_url).to_s
-
-    # # Download the image from the URL and save it to the destination file
-    # download_image(actual_image_url, image_filename)
-
-    # # Create a zip file and add the downloaded image to it
-    # create_zip_file(image_filename, zip_filename)
-
-    # puts "Logo downloaded and saved to #{zip_filename}"
-
   end
 
 
@@ -96,13 +83,13 @@ class StartupsController < ApplicationController
     File.open(destination, 'wb') { |file| file.write(image_data) }
   end
 
-  def create_zip_file(source_file, zip_file_name)
-    # Create a new zip file
-    Zip::ZipOutputStream.open(zip_file_name) do |zipfile|
-      # Add the image file to the zip file
-      zipfile.put_next_entry(File.basename(source_file))
-      File.open(source_file, 'rb') { |file| zipfile.write(file.read) }
-    end
-  end
+  # def create_zip_file(source_file, zip_file_name)
+  #   # Create a new zip file
+  #   Zip::ZipOutputStream.open(zip_file_name) do |zipfile|
+  #     # Add the image file to the zip file
+  #     zipfile.put_next_entry(File.basename(source_file))
+  #     File.open(source_file, 'rb') { |file| zipfile.write(file.read) }
+  #   end
+  # end
 
 end
